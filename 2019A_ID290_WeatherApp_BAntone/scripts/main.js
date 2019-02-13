86fd16063f62f1d890a31d2e8ec8585181707ee7
Breanna Antone <46906270+BrePotter1326@users.noreply.github.com>
2019-02-13 10:42:26 -0600

    Update main.js

--- 2019A_ID290_WeatherApp_BAntone/scripts/main.js
+++ 2019A_ID290_WeatherApp_BAntone/scripts/main.js
@@ -1,7 +1,7 @@
 $('#getBtn').click(function(){
     var search = $('#searchResults').val();
     //alert(search);
-    $.getJSON('https://api.openweathermap.org/data/2.5/weather?zip=' + $('#searchResults').val() + '&APPID=ffbaed99f10375462add667213061774',function(result){
+    $.getJSON('https://api.openweathermap.org/data/2.5/weather?zip=' + search + '&APPID=ffbaed99f10375462add667213061774',function(result){
         console.log(result);
         $('#title').html(String(data.id[0].name))
         
