a9bd32f3ccd27dee3bb5101b253c1c13d852bc3f
Breanna Antone <46906270+BrePotter1326@users.noreply.github.com>
2019-02-18 15:41:12 -0600

    Add files via upload

--- 2019A_ID290_WeatherApp_BAntone/scripts/main.js
+++ 2019A_ID290_WeatherApp_BAntone/scripts/main.js
@@ -3,7 +3,10 @@ $('#getBtn').click(function(){
     //alert(search);
     $.getJSON('https://api.openweathermap.org/data/2.5/weather?zip=' + search + '&APPID=ffbaed99f10375462add667213061774',function(result){
         console.log(result);
-        $('#title').html(String(data.id[0].name))
+        $('#title').html(String(result.name));
+        $('#temp').html(String(result.main.temp));
+        $('#country').html(String(result.sys.country));
+        $('#weather').html(String(result.weather[0].main))
         
     });
 });
